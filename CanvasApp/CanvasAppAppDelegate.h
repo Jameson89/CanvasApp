abcaa20616d991567f4cc1647d2119201d8b8124
atticus88 <atticus_88@yahoo.com>
2011-03-14 21:07:55 -0600

    Initial Commit.

---
+++ CanvasApp/CanvasAppAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  CanvasAppAppDelegate.h
+//  CanvasApp
+//
+//  Created by Klint Holmes on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CanvasAppViewController;
+
+@interface CanvasAppAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet CanvasAppViewController *viewController;
+
+@end
---
+++ CanvasApp/CanvasAppViewController.h
@@ -0,0 +1,15 @@
+//
+//  CanvasAppViewController.h
+//  CanvasApp
+//
+//  Created by Klint Holmes on 3/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CanvasAppViewController : UIViewController {
+    
+}
+
+@end
